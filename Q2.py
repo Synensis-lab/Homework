f515b285899e9ca6db44d212447d1f7502da75ac
Emma Gill <emmagill@gmail.com>
2022-09-03 17:21:39 +0100

    Q2 commit

---
+++ Q2.py
@@ -0,0 +1,32 @@
+# Simple ATM program
+# Using exception handling code blocks such as try/ except / else / finally, write a program that simulates an ATM machine to withdraw money.
+# (NB: the more code blocks the better, but try to use at least two key words e.g. try/except)
+# Tasks:
+# 1.       Prompt user for a pin code
+# 2.       If the pin code is correct then proceed to the next step, otherwise ask a user to type in a password again.
+#    You can give a user a maximum of 3 attempts and then exit a program.
+# 3.       Set account balance to 100.
+# 4.       Now we need to simulate cash withdrawal
+# 5.       Accept the withdrawal amount
+# 6.       Subtract the amount from the account balance and display the remaining balance (NOTE! The balance cannot be negative!)
+# 7.       However, when a user asks to ‘withdraw’ more money than they have on their account, then you need to raise an error an exit the program.
+
+uinput = input('Enter your 4 digit pin number with no spaces: ')
+pin = '1234'
+
+for number in range(2):
+    if uinput == pin:
+        print('Correct pin')
+        break
+    else:
+        print('Pin not correct. Try again')
+        uinput = input('Enter your 4 digit pin number with no spaces: ')
+
+if uinput != pin:
+    print('You did not enter correct pin. Your three tries are up!')
+    exit()
+
+start_balance = 100
+withdraw = int(input('Enter withdrawal amount: £'))
+current_balance = start_balance - withdraw
+print('Your balance is now: £{}'.format(current_balance))
