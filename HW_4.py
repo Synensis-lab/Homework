77b077eabf9ec234e07263e67e7a5c34d07aeba4
Emma Gill <emmagill@gmail.com>
2022-09-03 14:26:12 +0100

    Q1 inital commit

---
+++ HW_4.py
@@ -0,0 +1,16 @@
+# Question 1
+# Complete definitions for key Git & GitHub terminology
+# GIT WORKFLOW FUNDAMENTALS
+# ·        Working Directory
+# Working directory is the local file that is being currently worked on.
+# ·        Staging Area
+# Staging Area is also on the local machine, the files are being prepared to go to the repository. Files are moved from
+# working directory to the staging area.
+# ·        Local Repo (head)
+# The file from the staging area has been moved to the local repository. This means the local repository and local git repository are
+# the same.
+# ·        Remote repo (master)
+#The remote repository is on git hub. It is where the files are stored remotely. Files are pushed from local to remote and
+# pulled from remote to local.
+
+
